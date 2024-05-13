4d1d13f93fa4da3b4c08cf07d958f6bbeaa18e58
CHAMODYA NETHMINI HANDAPANGODA <135800562+Chamodyaa1@users.noreply.github.com>
2024-05-13 15:16:44 +0530

    FINAL PROJECT APP

--- app/src/main/java/com/example/taskmanager/Launch.kt
+++ app/src/main/java/com/example/taskmanager/Launch.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 
-class launch : AppCompatActivity() {
+class Launch : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
